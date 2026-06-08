80bb74fe7e0c427532f3d48582c082e5cd07b469
roadevopsGH <roadevops@gmail.com>
2026-06-08 04:09:54 -0500

    Update scripts.js
    Fixing page loading

--- scripts.js
+++ scripts.js
@@ -1,15 +1,6 @@
-/*This function shows the popup menu from the menu logo in
-the toolbar.  When a mouse hovers over the icon, it displays
-the menu.  When a mouse clicks on the icon again, it closes
-the menu.  When you are hovering over the div containing the
-menu, it will stay visible. when you click on one of the elements
-it will dissappear again.*/
-function openHomeMenu(){
-    var x = document.getElementById("toolbar-dropdown");
-    if (x.style.display == "none"){
-        x.style.display = "block";
+document.addEventListener('DOMContentLoaded', () => {
+    if (!sessionStorage.getItem('pageRefreshed')) {
+        sessionStorage.setItem('pageRefreshed', 'true');
+        window.location.reload();
     }
-    else{
-        x.style.display = "none";
-  }
-}
+});
