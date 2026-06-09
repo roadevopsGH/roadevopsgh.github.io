5e7735faa1222bef37ee1430947c591626107eb7
roadevopsGH <roadevops@gmail.com>
2026-06-09 01:03:37 -0500

    Delete scripts.js

--- scripts.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    if (!sessionStorage.getItem('pageRefreshed')) {
-        sessionStorage.setItem('pageRefreshed', 'true');
-        window.location.reload();
-    }
-});
