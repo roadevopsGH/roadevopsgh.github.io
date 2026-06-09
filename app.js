de34ea3b85f8c0ef0fd71ca6fa058c735f3b69ec
roadevopsGH <roadevops@gmail.com>
2026-06-09 01:56:25 -0500

    Update app.js
    Add searchbar

--- app.js
+++ app.js
@@ -1,14 +1,34 @@
+let helpData = [];
+
 fetch(`data.json?v=${Date.now()}`)
 .then(response => response.json())
 .then(items =>{
+    helpData = items;
+	renderCards(helpData);
+});
+
+function renderCards(items){
 	const container = document.getElementById("helpTopics");
 	container.innerHTML = items.map(item => `
-	<article class="help-card")>
-	<span class="badge">${item.category}</span>
+	    <article class="help-card")>
+	    <span class="badge">${item.category}</span>
         <h2>${item.issue}</h2>
         <ul>
           ${item.message.map(line => `<li>${line}</li>`).join("")}
         </ul>
       </article>
     `).join("");
+}
+
+document.getElementById("searchBox").addEventListner("input", event => {
+	const searchTerm = event.target.value.toLowerCase();
+
+	const filteredItems = helpData.filter(item => {
+		return(
+			item.issue.toLowerCase().includes(searchTerm) ||
+			item.category.toLowerCase().includes(searchTerm) ||
+			item.message.join(" ").toLoverCase().includes(searchTerm)
+		);
+	});
+	renderCards(filteredItems);
 });
