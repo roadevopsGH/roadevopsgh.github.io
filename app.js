5f02f907c1c9760c66be114cb824c7d4e69309f9
roadevopsGH <roadevops@gmail.com>
2026-06-09 01:05:04 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+fetch("data.json")
+.then(response => response.json())
+.then(items =>{
+	const container = document.getElementById("helpTopics");
+	container.innerHTML = items.map(item =>
+	<article class="help-card")>
+	<span class="badge">${item.category}</span>
+        <h2>${item.issue}</h2>
+        <ul>
+          ${item.message.map(line => `<li>${line}</li>`).join("")}
+        </ul>
+      </article>
+    `).join("");
+});
